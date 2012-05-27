2b988ed791a0cd07faa602605ffa1d0b963b3d50
syun77 <syun@syun.local>
2012-05-28 01:12:29 +0900

    Initial Commit

---
+++ Connect_X_Connect/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Connect_X_Connect
+//
+//  Created by OzekiSyunsuke on 12/05/28.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Connect_X_Connect/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Connect_X_Connect
+//
+//  Created by OzekiSyunsuke on 12/05/28.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
