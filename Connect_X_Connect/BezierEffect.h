6bd214329666e0c06fdfd78f00e6781ecd6e2cb6
syun77 <syun@syun.local>
2012-06-05 01:30:28 +0900

    ベジェエフェクトに変数を色々追加

--- Connect_X_Connect/BezierEffect.h
+++ Connect_X_Connect/BezierEffect.h
@@ -12,13 +12,25 @@
 #import "Vec.h"
 
 /**
+ * 種別 (目標)
+ */
+enum eBezierEffect {
+    eBezierEffect_Block,  // ブロック
+    eBezierEffect_Enemy,  // 敵
+    eBezierEffect_Player, // プレイヤー
+};
+
+/**
  * ベジェ曲線によるエフェクト
  */
 @interface BezierEffect : Token {
-    int     m_hTarget;
-    Vec2D   m_vList[60];
-    int     m_Timer;
-    int     m_Frame;
+    
+    eBezierEffect   m_Type;         // ターゲット種別
+    int             m_hTarget;      // ターゲットのハンドル（インデックス）
+    Vec2D           m_vList[60];    // ベジェ曲線用のベクトル配列
+    int             m_Timer;        // 経過時間
+    int             m_Frame;        // 移動フレーム数
+    int             m_Val;          // 汎用変数
 }
 
 - (void)setParam:(int)handle frame:(int)frame;
